c3803a1deb972b504d3e35b3591b989fc0d6a4ba
Gray <DrEarlGray@gmail.com>
2022-05-26 01:09:10 -0400

    Re-added Loop Ring

---
+++ WebRing/sites.js
@@ -0,0 +1,13 @@
+export const sites = [
+  'https://graycot.com/webring/',
+  'https://list.graycot.com',
+  'https://graystea.neocities.org/',
+  'https://windigo.neocities.org/',
+  'https://swiftyshq.neocities.org/',
+  'https://temaisgame.neocities.org/',
+  'https://vaea-v.neocities.org',
+  'https://www.idi.moe/'
+];
+
+
+
