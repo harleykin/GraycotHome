8aeefb4912e5a0743182e4ce302934ae7fb24305
Gray <DrEarlGray@gmail.com>
2022-05-28 01:21:41 -0400

    Change - Added and removed member site

--- WebRing/sites.js
+++ WebRing/sites.js
@@ -3,11 +3,11 @@ export const sites = [
   'https://list.graycot.com',
   'https://graystea.neocities.org/',
   'https://windigo.neocities.org/',
-  'https://swiftyshq.neocities.org/',
   'https://temaisgame.neocities.org/',
   'https://vaea-v.neocities.org',
   'https://www.idi.moe/',
-  'https://seirdy.one/'
+  'https://seirdy.one/',
+  'https://aroace.space/'
 ];
 
 
